d6d5a5128ac98ef9f76985dc46833e32754c5ee8
RemyAde <remysamade@gmail.com>
2024-03-16 01:27:54 +0100

    CREATE address table

---
+++ alembic/versions/0f84c921cd06_create_address_table.py
@@ -0,0 +1,32 @@
+"""create address table
+
+Revision ID: 0f84c921cd06
+Revises: 1217ad8e26b4
+Create Date: 2024-03-16 01:16:22.336973
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '0f84c921cd06'
+down_revision: Union[str, None] = '1217ad8e26b4'
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.create_table('address', 
+                    sa.Column('id', sa.Integer(), nullable=False, primary_key=True),
+                    sa.Column('address1', sa.String(), nullable=False),
+                    sa.Column('address2', sa.String(), nullable=False),
+                    sa.Column('city', sa.String(), nullable=False),
+                    sa.Column('state', sa.String(), nullable=False),
+                    sa.Column('postalcode', sa.String(), nullable=False))
+
+
+def downgrade() -> None:
+    op.drop_table('address')
